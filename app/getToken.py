a64a0a196a1024049e118be670b710f88fddf000
noMum <Fuck@you.com>
2022-04-23 14:25:39 +0800

    fix

--- app/getToken.py
+++ app/getToken.py
@@ -56,6 +56,10 @@ def piXiu(token_address):
     if result==None or len(result)!=2:
         print("unsellable")
         return 1
+    if result==[[0, 0],[0, 0]]:
+        print("unsellable")
+        return 1
+
     else:
         print(result)
         return 0
