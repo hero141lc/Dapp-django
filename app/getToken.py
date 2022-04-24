efd7084cd76919d95ec2c00f8495795d0d599643
noMum <Fuck@you.com>
2022-04-24 23:33:14 +0800

    s

--- app/getToken.py
+++ app/getToken.py
@@ -219,6 +219,7 @@ def getOrder(address):
     pixiuKing=[0,0,'']
 
     for item in data:
+        print("item-data:".item)
         if item['contractAddress']!=None and item['contractAddress'] in ban_token:
             continue
         if item['from'] == address:
