bc23f9fd614cd5426a94d9ab25556dc2c425003c
noMum <Fuck@you.com>
2022-04-24 17:39:09 +0800

    s

--- brickDapp/settings.py
+++ brickDapp/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'brickDapp.wsgi.application'
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
    }
-}'''
+}
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # 数据库引擎
