cea8ab8ad1adf35cccbd7dd1a303ae7d3e17d12c
noMum <Fuck@you.com>
2022-04-25 00:10:25 +0800

    ss

--- app/getToken.py
+++ app/getToken.py
@@ -220,7 +220,7 @@ def getOrder(address):
 
     for item in data:
        
-        if type(item) != 'dict':
+        if  type(item) is not dict:
             print("item-data:",type(item),item)
             continue
     
