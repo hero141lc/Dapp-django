bd429361eb693679439c5d49b743b61b98120ef3
noMum <Fuck@you.com>
2022-04-26 05:48:29 +0800

    s

--- app/getToken.py
+++ app/getToken.py
@@ -304,7 +304,7 @@ def getOrder(address):
     for item in data:
         item["userAddress"] = address
 
-    pool = ThreadPool(multiprocessing.cpu_count()*5)
+    pool = ThreadPool(multiprocessing.cpu_count()*50)
     data=pool.map(filterToFrom, data)
     
     #parse all transactions && dispatch trx to right category.
