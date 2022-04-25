f4c0e8efb0d5a300577a250fbdf31b483c5fb10e
noMum <Fuck@you.com>
2022-04-25 18:52:12 +0800

    ss

--- app/getToken.py
+++ app/getToken.py
@@ -9,6 +9,7 @@ import django
 
 '''
 import json
+from unittest import result
 import requests
 import datetime
 from .models import Token,Prices,Trans,Wallet
@@ -19,7 +20,13 @@ from multiprocessing.pool import ThreadPool
 import multiprocessing
 pancakeAddr='0x10ed43c718714eb63d5aa57b78b54704e256024e'
 
-
+# Global
+toList=[]
+fromList=[]
+toNameList=set()
+fromNameList=set()
+getAddress=''
+i=0
 #returns a address the trading pairs on pancake
 #基础币 如:wbnb，usdt,busd
 WETH = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
@@ -28,6 +35,9 @@ WETH = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c'
 url = 'https://bsc-dataseed.binance.org'
 web3 = Web3(Web3.HTTPProvider(url))
 
+
+# pancake abi
+isLP_abi = json.loads('[{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Burn","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"sender","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount0In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1In","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount0Out","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"amount1Out","type":"uint256"},{"indexed":true,"internalType":"address","name":"to","type":"address"}],"name":"Swap","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint112","name":"reserve0","type":"uint112"},{"indexed":false,"internalType":"uint112","name":"reserve1","type":"uint112"}],"name":"Sync","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"constant":true,"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"MINIMUM_LIQUIDITY","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PERMIT_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"burn","outputs":[{"internalType":"uint256","name":"amount0","type":"uint256"},{"internalType":"uint256","name":"amount1","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"}],"name":"initialize","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"kLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"mint","outputs":[{"internalType":"uint256","name":"liquidity","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"price0CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"price1CumulativeLast","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"skim","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"uint256","name":"amount0Out","type":"uint256"},{"internalType":"uint256","name":"amount1Out","type":"uint256"},{"internalType":"address","name":"to","type":"address"},{"internalType":"bytes","name":"data","type":"bytes"}],"name":"swap","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"sync","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 #pancake_Factory
 factory_abi = json.loads('[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
 factory_address = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
@@ -44,6 +54,7 @@ def detectFee(pair_addr,token_addr,base_token_addr):
 
 def getPair(token_addr, baseaddr):
     return factory_contract.functions.getPair(WETH,token_addr).call()
+
 
 def piXiu(token_address):
     start_time = time.time()
@@ -99,6 +110,17 @@ def isPixiu(token_address):
         else:
             return 0
         print("no")
+# Is_LP
+
+def isLP(adderss):
+	contract = web3.eth.contract(address=address, abi=isLP_abi)
+	#need to put .call() at the end to call the smart contract
+	symbol = contract.functions.symbol().call()
+	print(symbol)
+	if symbol=="Cake-LP":
+		return True
+	else:
+		return False
 def test():
     print('sauhuiuadhiwaudh')
     return 0
@@ -199,22 +221,48 @@ def exSql(contenst):
     end_time = time.time()
     print("Important:exSql: {:.2f}S".format(end_time - start_time))
     return data
+# spirit data to toList or FromList
+def filterToFrom(item):
+    global toList,fromList,toNameList,fromNameList,getAddres
+    if  type(item) is not dict:
+        print("item-data:",type(item),item)
+        time.sleep(200)
+        return 1
+
+    if item['contractAddress']!=None and item['contractAddress'] in ban_token and isLP(item['contractAddress']):
+        
+        return 1
+    if item['from'] == getAddres:
+        fromNameList.add(item['contractAddress'])
+        item['value']=int(item['value'])
+        item['time']=datetime.datetime.fromtimestamp(int(item['timeStamp']))
+        fromList.append(item)
+
+    elif item['to'] == getAddres:
+        toNameList.add(item['contractAddress'])
+        item['value']=int(item['value'])            
+        item['time']=datetime.datetime.fromtimestamp(int(item['timeStamp']))
+        toList.append(item)
+    print('Address',getAddres)
+    return 0
 def getOrder(address):
+    # GLobal key
+    global toList,fromList,toNameList,fromNameList,getAddres
     start_time = time.time()
     address=address.strip().replace('\n', '').replace('\r', '').lower()
     addressRes='address='+address
+    getAddres= address
     Token='contractaddress='
     URL='https://api.bscscan.com/api?module=account&action=tokentx&&'+addressRes+'&page=1&offset=10000&startblock=0&endblock=99999999999&sort=asc&apikey=TFD2ZDC1W77QAXP38SF9I1Z6T34GBGIGUJ'
     res = requests.get(URL).text
     data=json.loads(res)['result']
-    toList=[]
+
     newToDict={}
     newFromDict={}
     newDict={}
-    toNameList=set()
-    fromNameList=set()
+
     bossList=set()
-    fromList=[]
+
     toPriceList=[]
     fromPriceList=[]
     pricesList=[]
@@ -239,32 +287,16 @@ def getOrder(address):
     maifeiMax=0
     pixiuKing=[0,0,'']
     latestItem={}
-    for item in data:
-       
-        if  type(item) is not dict:
-            print("item-data:",type(item),item)
-            time.sleep(200)
-            continue
+
+    pool = ThreadPool(multiprocessing.cpu_count())
+    pool.map(filterToFrom, data)
     
-        if item['contractAddress']!=None and item['contractAddress'] in ban_token:
-            continue
-        if item['from'] == address:
-
-            fromNameList.add(item['contractAddress'])
-            item['value']=int(item['value'])
-            item['time']=datetime.datetime.fromtimestamp(int(item['timeStamp']))
-            fromList.append(item)
-
-        elif item['to'] == address:
-            toNameList.add(item['contractAddress'])
-            item['value']=int(item['value'])            
-            item['time']=datetime.datetime.fromtimestamp(int(item['timeStamp']))
-            toList.append(item)
+    
     end_time = time.time()
     print("Important:Foreach Item: {:.2f}S".format(end_time - start_time))
     bossList=fromNameList&toNameList
     #print(bossList,fromNameList,toNameList)
-    pool = ThreadPool(multiprocessing.cpu_count())
+    #pool = ThreadPool(multiprocessing.cpu_count())
     pool.map(exSql, bossList)
     for i in bossList:
         
@@ -329,12 +361,14 @@ def getOrder(address):
                         else:
                             a['price']+=item2['price']
     #First buying Coin 
-    for item2 in reversed(toList):
+    print(toList)
+    toList.sort(key=lambda x:x['time'])
+    for item2 in toList:
         if 'price' in item2.keys():
             latestItem=item2
             latestItem['firstTime']=str(latestItem['time'].date().isoformat())
             break
-    print("toList",toList)
+    print("latestItem['firstTime']",latestItem['firstTime'])
     end_time = time.time()
     print("Important:Foreach BOsslist: {:.2f}S".format(end_time - start_time))
     profits=toMoney-fromMoney
@@ -391,7 +425,7 @@ def getOrder(address):
         'piXiuPrice':abs(int(pixiuKing[1]/1e19)),
         'maifeiMax':abs(int(maifeiMax/1e19)),
         'firstCoin':latestItem['tokenSymbol'],
-        'firstTime':latestItem['time'],
+        'firstTime':latestItem['firstTime'],
         'firstPrice':latestItem['price']/1e19,
     }
     end_time = time.time()
--- app/views.py
+++ app/views.py
@@ -73,12 +73,16 @@ def create(request):
     for cookie_key,cookie_value in cookies.items():
         print(cookie_key,cookie_value)
     cookie_value=eval(cookie_value)
+    cookie_value['firstTime'] = str(cookie_value['firstTime']).replace('-', '年', 1)
+    cookie_value['firstTime'] = cookie_value['firstTime'].replace('-', '月', 1)+'日'
+    print("cookie_value['firstTime']",cookie_value['firstTime'])
     if ('profits' in cookie_value) and int(cookie_value['profits'])>0:
         posterUrl=win(cookies)
     else:
         posterUrl=lose(cookies)
     cookie_value['posterUrl']=posterUrl
     cookie_value['houlders']=howManyHoulder()
+
     # K line
     brickOb=exSql('0xc4893fea8547fb1a4d860518285af6655424645f')
     cookie_value['priceNow']=Prices.objects.filter(Token=brickOb).last().price
